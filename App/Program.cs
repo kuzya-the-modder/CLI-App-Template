68a86382e42d9d1c7e41828135756f0ed1eba7c6
Kuzya <kuzyathemodder@gmail.com>
2024-05-15 07:41:31 +0300

    Add project files.

---
+++ App/Program.cs
@@ -0,0 +1,32 @@
+﻿using Kuzya.CLI;
+using Kuzya;
+
+public static class Program
+{
+    static Tree command;
+
+    static void Main(string[] argvs)
+    { Args args = Args.New(argvs);
+        #region Init
+        // intern name, seen only in code (better set a name that app will be published as a.k.a: cliapp -> cliapp.exe)
+        command = new Tree("cliapp");
+        // *cliapp* node
+        command.AddSub(new Node("node")
+            .SetAct((_) => { Console.WriteLine("node!"); })
+            .AddSub(new Node("world")
+                .SetAct((_) => { Console.WriteLine("Hello, World!"); }))
+            );
+        command.SetAct((args) => 
+            { // better wrap in functions, not lambas
+                if (args.HasFlag("help", "h"))
+                { 
+                    Console.WriteLine("help article"); 
+                    return; 
+                }
+                Console.WriteLine("from test"); });
+        #endregion
+        #region Call
+        command.Call(args);
+        #endregion
+    }
+}
---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3e1f6c8f-5c8e-49fd-b304-061e9b577ff6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
